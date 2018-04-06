e2edf8f0a65e707356da2bcfbd3b3d8381ed7aed
Chandan Kumar <CKUMAR2@hallmark.com>
2018-04-06 11:26:40 -0500

    Add project files.

---
+++ XMLSerialization/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Xml.Serialization;
+using XMLSerialization;
+public class Test
+{
+    static void Main()
+    {
+        chandan c = new chandan();
+
+
+        Deserialise();
+
+
+        c.FirstName = "chandan";
+        c.LastName = "Kumar";
+
+        chandan.Details myDetails = new chandan.Details();
+        myDetails.Y.Add("myData1");
+        myDetails.Y.Add("myData2");
+
+        c.MyDetails = myDetails;
+
+        //XmlSerializer x = new XmlSerializer(typeof(DataCollections));  
+        XmlSerializer x = new XmlSerializer(typeof(chandan));
+         TextWriter txtWriter = new StreamWriter("outPut.xml");
+        //x.Serialize(txtWriter, listInfo); 
+        x.Serialize(txtWriter, c);
+
+
+
+
+
+    }
+
+
+    public static void Deserialise()
+
+    {
+        chandan c = new chandan();
+        XmlSerializer serializer = new XmlSerializer(typeof(chandan));
+
+        StreamReader reader = new StreamReader("outPut" + ".xml");
+
+    //    TextReader reader = new StreamReader("outPut" + ".xml");
+        c = (chandan)serializer.Deserialize(reader);
+    }
+
+}
---
+++ XMLSerialization/chandan.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    public class chandan
+    {
+        private Details myDetails;
+
+
+        [XmlRoot("Details")]
+        public class Details
+        {
+            private List<string> y = new List<string>();
+            [XmlArray("ItemsofCD")]
+            public List<string> Y
+            {
+
+                get
+                {
+
+                    return y;
+                }
+
+                set
+                {
+                    y = value;
+                }
+            }
+        }
+
+        private string firstName;
+
+        private string lastName;
+
+        
+
+        [XmlElement("First-Name")]
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        [XmlElement("Last-Name")]
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public Details MyDetails
+        {
+            get
+            {
+                return myDetails;
+            }
+
+            set
+            {
+                myDetails = value;
+            }
+        }
+    }
+}
